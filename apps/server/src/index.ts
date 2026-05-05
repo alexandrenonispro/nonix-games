993f5144ac1ef189e5249436d7c6b37d674267b0
Alexandre Nonis <alexandrenonispro@gmail.com>
2026-05-05 21:29:45 +0200

    fix

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -3,11 +3,11 @@ import { createServer } from 'http'
 import { app } from './api/app.js'
 import { initSocketIO } from './socket/index.js'
 
-const PORT = process.env.PORT ?? 3001
+const PORT = parseInt(process.env.PORT ?? '3001', 10)
 const httpServer = createServer(app)
 
 initSocketIO(httpServer)
 
-httpServer.listen(PORT, '0.0.0.0', () => {
+httpServer.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`)
 })
